61fc47552e8c8f2c8942f57bd12fec3ea09bb338
Matheus Leite <matheusjoselfm@gmail.com>
2024-07-20 18:28:22 -0300

    fix: translate weather api response to portuguese

--- src/weather/client.py
+++ src/weather/client.py
@@ -1,7 +1,7 @@
 import requests
 
-URL = "https://wttr.in/{city}?format=j1"
+URL = "https://wttr.in/{city}?format=j1&lang=pt"
 
 def get_weather(city: str) -> str:
     response = requests.get(URL.format(city=city))
-    return response.json().get("current_condition")[0].get("weatherDesc")[0].get("value")
+    return response.json().get("current_condition")[0].get("lang_pt")[0].get("value")
