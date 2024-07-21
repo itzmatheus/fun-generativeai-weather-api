381e2e26af5ec2560bb560a4abd5cbfec1b67786
Matheus Leite <matheusjoselfm@gmail.com>
2024-07-21 14:30:27 -0300

    test: shouldnt generate question when weather api fails

--- src/city/test_routes.py
+++ src/city/test_routes.py
@@ -8,22 +8,37 @@ client = TestClient(app)
 class MockGenerativeAIText(GenerativeAIText):
     def generate_text(self, prompt: str, system_prompt: str = None, max_tokens: int = 200):
         return f"Generated text mock"
-    
+
+def override_get_chatgpt_generative_ai_text() -> GenerativeAIText:
+    return MockGenerativeAIText()    
+
 class MockWeatherApi(WeatherApi):
     def get_weather(self, location: str) -> str:
         return WeatherDTO(description="Sunny")
     
-def override_get_chatgpt_generative_ai_text() -> GenerativeAIText:
-    return MockGenerativeAIText()
-
 def override_get_wttr_weather_api() -> WeatherApi:
     return MockWeatherApi()
 
-app.dependency_overrides[get_chatgpt_generative_ai_text] = override_get_chatgpt_generative_ai_text
-app.dependency_overrides[get_wttr_weather_api] = override_get_wttr_weather_api
+class MockWeatherApiFailure(MockWeatherApi):
+    def get_weather(self, location: str):
+        raise Exception("Weather API failure")
+
+def override_get_wttr_weather_api_failure() -> WeatherApi:
+    return MockWeatherApiFailure()
 
 def test_should_generate_question_success():
+    app.dependency_overrides[get_chatgpt_generative_ai_text] = override_get_chatgpt_generative_ai_text
+    app.dependency_overrides[get_wttr_weather_api] = override_get_wttr_weather_api
+
     response = client.post("/city/question_generate", json={"entrada": "Pianco"})
     
     assert response.status_code == 200
     assert response.json() == {"saida": "Generated text mock"}
+
+def test_shouldnt_generate_question_when_weather_api_fails():    
+    app.dependency_overrides[get_wttr_weather_api] = override_get_wttr_weather_api_failure
+
+    response = client.post("/city/question_generate", json={"entrada": "Pianco"})
+    
+    assert response.status_code == 500
+    assert response.json() == {"detail": "Weather API failure"}
