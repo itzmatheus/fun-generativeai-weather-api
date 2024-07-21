3110f696dbb7ce1afcd6c7f5050ce5c885825de8
Matheus Leite <matheusjoselfm@gmail.com>
2024-07-21 14:33:45 -0300

    test: shouldnt generate question when generativeai api fails

--- src/city/test_routes.py
+++ src/city/test_routes.py
@@ -12,6 +12,13 @@ class MockGenerativeAIText(GenerativeAIText):
 def override_get_chatgpt_generative_ai_text() -> GenerativeAIText:
     return MockGenerativeAIText()    
 
+class MockGenerativeAITextFailure(GenerativeAIText):
+    def generate_text(self, prompt: str, system_prompt: str = None, max_tokens: int = 200):
+        raise Exception("Generative AI failure")
+
+def override_generativeai_text_failure() -> GenerativeAIText:
+    return MockGenerativeAITextFailure()    
+
 class MockWeatherApi(WeatherApi):
     def get_weather(self, location: str) -> str:
         return WeatherDTO(description="Sunny")
@@ -42,3 +49,12 @@ def test_shouldnt_generate_question_when_weather_api_fails():
     
     assert response.status_code == 500
     assert response.json() == {"detail": "Weather API failure"}
+
+def test_shouldnt_generate_question_when_generative_ai_fails():
+    app.dependency_overrides[get_wttr_weather_api] = override_get_wttr_weather_api
+    app.dependency_overrides[get_chatgpt_generative_ai_text] = override_generativeai_text_failure
+
+    response = client.post("/city/question_generate", json={"entrada": "Pianco"})
+    
+    assert response.status_code == 500
+    assert response.json() == {"detail": "Generative AI failure"}
