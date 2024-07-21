24bea1f649903bdcf02b289f5d56f7f3783ce529
Matheus Leite <matheusjoselfm@gmail.com>
2024-07-21 18:01:57 -0300

    ci: load env from system or .env file

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,3 +1,10 @@
 import os
+from pathlib import Path
+from starlette.config import Config
 
-OPEN_AI_KEY = os.getenv("OPENAI_API_KEY")
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+env_path = os.path.join(BASE_DIR, ".env")
+config = Config(env_path)
+
+OPEN_AI_KEY = os.getenv("OPENAI_API_KEY") or config("OPENAI_API_KEY")
