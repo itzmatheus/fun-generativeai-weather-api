f680dac7ebcce50eb09cb11a04ba6fb929ba264c
Matheus Leite <matheusjoselfm@gmail.com>
2024-07-21 17:31:19 -0300

    ci: change way to project get env in docker compose

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,10 +1,3 @@
 import os
-from pathlib import Path
-from starlette.config import Config
 
-
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-env_path = os.path.join(BASE_DIR, ".env")
-config = Config(env_path)
-
-OPEN_AI_KEY = config("OPENAI_API_KEY")
+OPEN_AI_KEY = os.getenv("OPENAI_API_KEY")
